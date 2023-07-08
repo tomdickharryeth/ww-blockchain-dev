a5d7d9de1f7166c99be21c7794d655bbdd106df9
TDH <wilderworldswallet@gmail.com>
2023-07-08 20:53:19 +0100

    update notebook

--- session2/hardhat.config.js
+++ session2/hardhat.config.js
@@ -11,11 +11,15 @@ const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.15",
-   defaultNetwork: "goerli",
+   defaultNetwork: "sepolia",
    networks: {
     hardhat: {},
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
     },
      ethereum: {
